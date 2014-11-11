62d48abd487166acf5433f20a73c15426af1c3f8
Tim Rodger <tim.rodger@gmail.com>
2014-11-11 00:10:00 +0000

    Updates routes to take post requests

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
@@ -7,17 +9,19 @@ $app = new Silex\Application();
 $pub_key_file = __DIR__.'/../config/mykey.pub';
 $priv_key_file = __DIR__.'/../config/mykey.pem';
 
-$app->get('/encrypt/{string}', function($string) use($app, $pub_key_file) {
+$app->post('/encrypt', function(Request $request) use($app, $pub_key_file) {
     $key_text = file_get_contents($pub_key_file);
+    $string = $request->get('string');
     openssl_public_encrypt($string, $crypt_text, $key_text);
-    return $app->json(['encrypted' => base64_encode($crypt_text)]); 
+    return $app->json(['encrypted' => base64_encode($crypt_text), 'plain_text' => $string]); 
 });
 
-$app->get('/decrypt/{string}', function($string) use($app, $priv_key_file) { 
+$app->post('/decrypt', function(Request $request) use($app, $priv_key_file) { 
     $key_text = file_get_contents($priv_key_file);
     $private_key = openssl_get_privatekey($key_text);
+    $string = $request->get('string');
     openssl_private_decrypt(base64_decode($string), $decrypted, $private_key);
-    return $app->json(['plain_text' => $decrypted]);
+    return $app->json(['plain_text' => $decrypted, 'encrypted' => $string]);
 });
 
 $app->run();
