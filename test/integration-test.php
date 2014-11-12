5ae56377dbcc71bd9d021320589ceb647747b60a
Tim Rodger <tim.rodger@gmail.com>
2014-11-12 20:27:37 +0000

    Adds a test that encrypted strings are decrypted correctly

---
+++ test/integration-test.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$server = $argv[1];
+$string = 'my text to encrypt';
+
+$encrypt_endpoint = sprintf('%s/encrypt', $server);
+$decrypt_endpoint = sprintf('%s/decrypt', $server);
+
+$client = new Client();
+$response = $client->post($encrypt_endpoint, ['body' => ['string' => $string]]);
+
+$encrypted_data = $response->json();
+var_dump($encrypted_data);
+
+$response = $client->post($decrypt_endpoint, ['body' => ['string' => $encrypted_data['encrypted']]]);
+
+$decrypted_data = $response->json();
+
+var_dump($decrypted_data);
+
+if ($string === $decrypted_data['plain_text']){
+    print "Success\n";
+} else {
+    print "Failure\n";
+}
